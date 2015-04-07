26637911577ac65f91f1b37459c70630f87d13d6
benjanisa <benjimeltzer@gmail.com>
2015-04-07 14:28:37 +0200

    Updated to use ee_survey2 (handles embedded structure)

--- client/templates/index.js
+++ client/templates/index.js
@@ -14,189 +14,49 @@ var provinceJSON = {"type": "FeatureCollection","features": [
 ]};
 
 Template.dashboard.rendered = function(){
-	var remote = DDP.connect('http://ee_survey.meteor.com/');
+	var remote = DDP.connect('http://ee_survey2.meteor.com/');
 	Schools = new Meteor.Collection('schools', remote);
 	console.log('Getting schools');
 	remote.subscribe('schools', function() {
 		console.log('SUBSCRIBED TO SCHOOLS DB, list of schools:')
 	  var schools = Schools.find().fetch();
-
-
-	  // console.log(JSON.stringify(schools));
-	  // var json = Schools.find({'_id' : this.params._id }).fetch(); // what ever data you want to return
-      // this.response.setHeader('Content-Type', 'application/json');
-      // this.response.end(JSON.stringify(json));
-
-      console.log(schools)
-			//pause here!
-	  	makeGraphs(null, schools, provinceJSON);
-
-	});
-	Grades = new Meteor.Collection('grades', remote);
-	console.log('Getting grades');
-	remote.subscribe('grades', function() {
-		console.log('SUBSCRIBED TO GRADES DB, list of grades:')
-		var grades = Grades.find().fetch();
-
-
-		// console.log(JSON.stringify(schools));
-		// var json = Schools.find({'_id' : this.params._id }).fetch(); // what ever data you want to return
-			// this.response.setHeader('Content-Type', 'application/json');
-			// this.response.end(JSON.stringify(json));
-
-			console.log(grades);
-
-	});
-	Versions = new Meteor.Collection('surveyVersions', remote);
-	console.log('Getting versions');
-	remote.subscribe('surveyVersions', function() {
-		console.log('SUBSCRIBED TO VERSIONS DB, list of versions:')
-		var versions = Versions.find().fetch();
-// 		var grades = Grades.find().fetch();
-//
-// 		// console.log(JSON.stringify(schools));
-// 		// var json = Schools.find({'_id' : this.params._id }).fetch(); // what ever data you want to return
-// 			// this.response.setHeader('Content-Type', 'application/json');
-// 			// this.response.end(JSON.stringify(json));
-//
-// 			console.log(versions);
-// 			mapVersions = function() {
-//     var values = {
-//         name: this.name
-//     };
-//     emit(this._id, values);
-// };
-// mapGrades = function() {
-//     var values = {
-//         gradeId: this._id,
-//         totalTeachersInSchool: this.totalTeachersInSchool
-//     };
-//     emit(this.versionId, values);
-// };
-// reduce = function(k, values) {
-//     var result = {}, gradeFields = {
-//         "gradeId": '',
-//         "totalTeachersInSchool": ''
-//     };
-//     values.forEach(function(value) {
-//         var field;
-//         if ("grade" in value) {
-//             if (!("grades" in result)) {
-//                 result.grades = [];
-//             }
-//             result.grades.push(value);
-//         } else if ("grades" in value) {
-//             if (!("grades" in result)) {
-//                 result.grades = [];
-//             }
-//             result.grades.push.apply(result.grades, value.grades);
-//         }
-//         for (field in value) {
-//             if (value.hasOwnProperty(field) && !(field in gradeFields)) {
-//                 result[field] = value[field];
-//             }
-//         }
-//     });
-//     return result;
-// };
-// var versions_grades
-// Versions.mapReduce(mapVersions, reduce, {"out": {"reduce": "versions_grades"}});
-// Grades.mapReduce(mapGrades, reduce, {"out": {"reduce": "versions_grades"}});
-// console.log(versions_grades)
-			//LINK VERSIONS AND GRADES
-
+    console.log(schools)
+	  makeGraphs(null, schools, provinceJSON);
 
 	});
 
 
 }
 
-//DATATABLE METHOD
-// jQuery(document).ready(function ($) {
-//         $('#tabs').tab();
-//     });
-// $("#table-search").on('input',function(){
-//    text_filter(nameDim,this.value);//companyDimension is the dimension for the data table
-
-// function text_filter(dim,q){
-//  dataTable.filterAll();
-//  var re = new RegExp(q,"i")
-//  if (q != '') {
-//      dim.filter(function(d) {
-//          return 0 == d.search(re);
-//      });
-//  } else {
-//      dim.filterAll();
-//  }
-// dc.redrawAll();
-// //graphCustomizations();
-//   }});
-// queue()
-//     .defer(d3.json, "/donorschoose/projects")
-//     .defer(d3.json, "static/geojson/za-states.json")
-//     .await(makeGraphs);
-
 function makeGraphs(error, projectsJson, statesJson) {
 
-        //Clean projectsJson data
+        //load school data
         var schools = projectsJson;
-
-        //var dateFormat = d3.time.format("%Y-%m-%d");
-        //donorschooseProjects.forEach(function(d) {
-        //      d["date_posted"] = dateFormat.parse(d["date_posted"]);
-        //      d["date_posted"].setDate(1);
-        //      d["total_donations"] = +d["total_donations"];
-        //});
 
         //Create a Crossfilter instance
         var ndx = crossfilter(schools);
         //Define Dimensions
-  // console.log("SCHOOLS")
-  //console.log(schools)
-        nameDim = ndx.dimension(function(d) { return
-
-    d["schoolDetails"]["INSTITUTION_NAME"];
-    });
-
-        //var typeDim = ndx.dimension(function(d) { return d["type"]; });
-        //var povertyLevelDim = ndx.dimension(function(d) { return d["poverty_level"]; });
-        var stateDim = ndx.dimension(
-    function(d) {
-      // console.log("DATA")
-        // console.log(d);
-        // console.log(d["_id"]);
-        // console.log(d["schoolDetails"]["PROVINCE_NAME"]);
-
-        return d["schoolDetails"]["PROVINCE_NAME"];
-    });
-  //console.log("STATE DIM")
-  //console.log(stateDim)
-  var nameDim = ndx.dimension(function(d) { return d["schoolDetails"]["INSTITUTION_NAME"]; });
-        //var totalDonationsDim  = ndx.dimension(function(d) { return d["total_donations"]; });
-
+        var nameDim = ndx.dimension(function(d) {
+					return d["INSTITUTION_NAME"];
+				});
+        var stateDim = ndx.dimension(function(d) {
+        	return d["PROVINCE_NAME"];
+    		});
+				var gradeDim = ndx.dimension(function(d) {
+					return d["Surveys"]["Grades"];
+				});
 
         //Calculate metrics
-        //var numProjectsByDate = dateDim.group();
-        //var numProjectsByResourceType = resourceTypeDim.group();
-        //var numProjectsByPovertyLevel = povertyLevelDim.group();
-        //var totalDonationsByState = stateDim.group().reduceSum(function(d) {
-                //return d["total_donations"];
-        //});
+				var totalSchoolsByProvince = stateDim.group().reduceCount(function(d) {
+					// console.log(d["schoolDetails"]["INSTITUTION_NAME"]);
+					return d["INSTITUTION_NAME"];
+				});
+				//Define values (to be used in charts)
+				var max_province = totalSchoolsByProvince.top(1)[0].value;
 
-        //var all = ndx.groupAll();
-        //var totalDonations = ndx.groupAll().reduceSum(function(d) {return d["total_donations"];});
 
-        //var max_state = totalDonationsByState.top(1)[0].value;
-
-        //Define values (to be used in charts)
-        //var minDate = dateDim.bottom(1)[0]["date_posted"];
-        //var maxDate = dateDim.top(1)[0]["date_posted"];
-
-    //Charts
-//  var yearRingChart = dc.pieChart("#pie-chart");
-  //var rsaChart = dc.geoChoroplethChart("#map");
-  //console.log(projectsJson)
-  var datatable =$("#example").dataTable({
+    		//Datatable
+  			var datatable =$("#example").dataTable({
             //"bPaginate": false,
             //"bLengthChange": false,
             //"bFilter": false,
@@ -207,103 +67,50 @@ function makeGraphs(error, projectsJson, statesJson) {
             "aaData": nameDim.top(Infinity),
             "bDestroy": true,
             "aoColumns": [
-                { "mData": "schoolDetails.INSTITUTION_NAME", "sDefaultContent": " "},
-                { "mData": "schoolDetails.CLASSIFICATION", "sDefaultContent": " "},
-                { "mData": "schoolDetails.PROVINCE_NAME", "sDefaultContent": " "},
-                { "mData": "schoolDetails.TOWN_OR_CITY", "sDefaultContent": " "},
-                { "mData": "schoolDetails.DISTRICT_NAME", "sDefaultContent": " "},
-                { "mData": "schoolDetails.TELEPHONE_NO", "sDefaultContent": " " },
-                { "mData": "schoolDetails.NEIMS_NUMBER", "sDefaultContent": " " }
+                { "mData": "INSTITUTION_NAME", "sDefaultContent": " "},
+                { "mData": "CLASSIFICATION", "sDefaultContent": " "},
+                { "mData": "PROVINCE_NAME", "sDefaultContent": " "},
+                { "mData": "TOWN_OR_CITY", "sDefaultContent": " "},
+                { "mData": "DISTRICT_NAME", "sDefaultContent": " "},
+                { "mData": "TELEPHONE_NO", "sDefaultContent": " " },
+                { "mData": "NEIMS_NUMBER", "sDefaultContent": " " }
             ]
         });
-        //REFRESH THE TABLE
 
-  var usChart = dc.geoChoroplethChart("#sa-map");
-  var studentPlot = dc.rowChart("#studentPlot");
-  var totalStudentsBySchool = nameDim.group().reduceSum(function(d) {
-    return d["schoolDetails"]["NEIMS_NUMBER"];
-  });
-  var totalSchoolsByProvince = nameDim.group().reduceSum(function(d) {
-    return d["schoolDetails"]["PROVINCE_NAME"];
-  });
-  var totalDonationsByState = stateDim.group().reduceSum(function(d) {
-    return d["schoolDetails"]["NEIMS_NUMBER"];
-  });
-  var totalSchoolsByState = stateDim.group().reduceCount(function(d) {
-    // console.log(d["schoolDetails"]["INSTITUTION_NAME"]);
-    return d["schoolDetails"]["INSTITUTION_NAME"];
-  });
-  var max_state = totalSchoolsByState.top(1)[0].value;
-  //console.log("TOTAL SCHOOLS BY STATE");
-  //console.log(totalSchoolsByState)
-  //var spend = typeDim.group().reduceSum(function (d) {
-    //        return +d.male;
-      //  }),
-      /*
-        dataTable = dc.dataTable("#resource-type-row-chart");
-        dataTable
-        .width(600)
-        .height(250)
-        .dimension(nameDim)
-        .group(function(d) { return "School Info"
-         })
-        .size(5)
-    .columns([
-      function(d) { return d["name"]; },
-      function(d) { return d["type"]; },
-      function(d) { return d["Suburb"]; },
-      function(d) { return d["District"]; },
-      function(d) { return d["EMIS"]; },
-      function(d) { return d["male"]; },
-      function(d) { return d["female"]; },
-      function(d) { return d["total"]; },
-      function(d) { return d["teachers"]; },
-    ])
-    .sortBy(function(d){ return d["name"]; })
-    .order(d3.ascending);*/
+				//Charts
+  			var usChart = dc.geoChoroplethChart("#sa-map");
+  			var studentPlot = dc.rowChart("#studentPlot");
 
-    studentPlot
+    		studentPlot
           .width(300)
           .height(330)
           .dimension(stateDim)
           .group(totalSchoolsByProvince)
           .elasticX(true);
 
-    usChart.width(1000)
-      .height(330)
-      .dimension(stateDim)
-      .group(totalSchoolsByState)
-      .colors(['#ccc', '#E2F2FF','#C4E4FF','#9ED2FF','#81C5FF','#6BBAFF','#51AEFF','#36A2FF','#1E96FF','#0089FF','#0061B5'])
-      //.colors(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])
-      .colorDomain([0, 10])
-      .legend(dc.legend().x(-180).y(-580).itemHeight(13).gap(5))
-      .overlayGeoJson(statesJson["features"], "state", function (d) {
-        return d.properties.name;
-      })
+    	usChart.width(1000)
+		      .height(330)
+		      .dimension(stateDim)
+		      .group(totalSchoolsByProvince)
+		      .colors(['#ccc', '#E2F2FF','#C4E4FF','#9ED2FF','#81C5FF','#6BBAFF','#51AEFF','#36A2FF','#1E96FF','#0089FF','#0061B5'])
+		      //.colors(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])
+		      .colorDomain([0, 10])
+		      .legend(dc.legend().x(-180).y(-580).itemHeight(13).gap(5))
+		      .overlayGeoJson(statesJson["features"], "state", function (d) {
+		        return d.properties.name;
+		      	})
       //.projection(d3.geo.equirectangular()
         //    .scale(153)
           //  .translate(340,150)
             //.precision(.1))
-      .projection(d3.geo.azimuthalEqualArea()
-          .clipAngle(180 - 1e-3)
-          .scale(1500)
-          .translate([-200, -600])
-          .precision(.1))
-      /*.title(function (p) {
-        return "State: " + p["key"]
-            + "\n"
-            + "Total Donations: " + Math.round(p["value"]) + " $";
-      })*/
-/*
-    yearRingChart.width(200).height(200)
-        .dimension(typeDim)
-        .group(spend)
-        .innerRadius(50);
-    console.log("HELLO")
-    console.log(nameDim)
-    console.log(spend)
-    */
-    function RefreshTable() {
+	      .projection(d3.geo.azimuthalEqualArea()
+	          .clipAngle(180 - 1e-3)
+	          .scale(1500)
+	          .translate([-200, -600])
+	          .precision(.1))
+
+				//function to refresh datatable
+    		function RefreshTable() {
               dc.events.trigger(function () {
                   alldata = nameDim.top(Infinity);
                   //console.log("REFRESH TABLE!")
@@ -318,6 +125,7 @@ function makeGraphs(error, projectsJson, statesJson) {
               var chartI = dc.chartRegistry.list()[i];
               chartI.on("filtered", RefreshTable);
           }
+		//render charts
     dc.renderAll();
 
 };
